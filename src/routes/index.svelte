da2bca55df42b40051be7046c9c527baaa8afdf5
Arlen Peiffer <arlenpeiffer@gmail.com>
2021-01-10 18:03:56 -0800

    updates index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,66 @@
 <script>
 	import jockjams from 'images/jockjams.jpg';
+
+	let links = [
+		{ href: 'https://github.com/arlenpeiffer', text: 'Github' },
+		{ href: 'https://twitter.com/arlenpeiffer', text: 'Twitter' },
+		{ href: 'https://bean.la', text: 'Bean.la' }
+	];
 </script>
 
 <style lang="scss">
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+	@import "./style/main.scss";
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-		@media (min-width: 480px) {
-			font-size: 4em;
-		}
-	}
+	main {
+		display: grid;
+		gap: 2.5rem;
+		max-width: 25rem;
+		padding: 4rem 2rem;
 
-	figure {
-		margin: 0 0 1em 0;
 		img {
 			width: 100%;
-			max-width: 400px;
-			margin: 0 0 1em 0;
 		}
-	}
 
-	p {
-		margin: 1em auto;
+		h1 {
+			font-size: $font-size;
+			text-align: center;
+		}
+
+		ul {
+			display: inline-grid;
+			grid-auto-flow: column;
+			gap: 3rem;
+			place-items: center;
+			margin: auto;
+			@include breakpoint(mobile) {
+				gap: 4rem;
+			}
+		}
+
+		a {
+			border-bottom: 1px solid $black;
+			&:hover {
+				opacity: 0.75;
+			}
+		}
 	}
 </style>
 
 <svelte:head>
 	<title>Hi. I’m Arlen. I help out with the internet.</title>
+	<meta name="description" content="A personal site for the person Arlen Peiffer who does web development things" />
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Smiley kid holding an unopened copy of Jock Jams, Volume 1 on compact disc" src="{jockjams}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<main>
+	<img alt="Smiley kid holding an unopened copy of Jock Jams Volume 1 on compact disc" src="{jockjams}">
+	<h1>Hi. I’m Arlen. I help out with the internet.</h1>
+	<ul>
+		{#each links as link}
+			<li>
+				<a href="{link.href}" target="_blank">
+					{link.text}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</main>
